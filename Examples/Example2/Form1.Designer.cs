9fd679df513a66f2418215f0e8ed7350159bf1c5
Ehsan Kamali <EhsanKamali@users.noreply.github.com>
2021-01-14 17:40:03 +0330

    Add Example2

---
+++ Examples/Example2/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Example2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelSalary = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSalary = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(63, 59);
+            this.labelName.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(87, 24);
+            this.labelName.TabIndex = 5;
+            this.labelName.Text = "نام را وارد کنید:";
+            // 
+            // labelSalary
+            // 
+            this.labelSalary.AutoSize = true;
+            this.labelSalary.Location = new System.Drawing.Point(63, 133);
+            this.labelSalary.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelSalary.Name = "labelSalary";
+            this.labelSalary.Size = new System.Drawing.Size(131, 24);
+            this.labelSalary.TabIndex = 6;
+            this.labelSalary.Text = "مقدار حقوق را وارد کنید:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(268, 54);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(281, 31);
+            this.textBoxName.TabIndex = 0;
+            this.textBoxName.Text = "Saba";
+            // 
+            // textBoxSalary
+            // 
+            this.textBoxSalary.Location = new System.Drawing.Point(268, 133);
+            this.textBoxSalary.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.textBoxSalary.Name = "textBoxSalary";
+            this.textBoxSalary.Size = new System.Drawing.Size(281, 31);
+            this.textBoxSalary.TabIndex = 1;
+            this.textBoxSalary.TextChanged += new System.EventHandler(this.textBoxSalary_TextChanged);
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(68, 203);
+            this.buttonCalculate.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(153, 58);
+            this.buttonCalculate.TabIndex = 2;
+            this.buttonCalculate.Text = "C&alculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(235, 203);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(153, 58);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "&Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(63, 284);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(61, 24);
+            this.labelResult.TabIndex = 7;
+            this.labelResult.Text = "Result:";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(398, 203);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(153, 58);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.Text = "E&xit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 373);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxSalary);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelSalary);
+            this.Controls.Add(this.labelName);
+            this.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "Calculate Salary Tax";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelSalary;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSalary;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Examples/Example2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Example2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CalculateEnable();
+        }
+
+        private void CalculateEnable()
+        {
+            buttonCalculate.Enabled = Convert.ToBoolean(textBoxSalary.Text.Length);
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            int Salary;
+            string Result;
+            int SalaryTax;
+
+            int.TryParse(textBoxSalary.Text, out Salary);
+
+            if (Salary < 500000)
+            {
+                SalaryTax = Salary * 0;
+            }
+            else
+            if (Salary < 1000000)
+            {
+                SalaryTax = Salary * 5 / 100;
+            }
+            else
+            if (Salary < 2000000)
+            {
+                SalaryTax = Salary * 10 / 100;
+            }
+            else
+                if (Salary < 3000000)
+            {
+                SalaryTax = Salary * 15 / 100;
+            }
+            else
+            {
+                SalaryTax = Salary * 20 / 100;
+            }
+
+            Result ="\n"+ textBoxName.Text + 
+                " your tax is = " + SalaryTax.ToString() + "T"+
+                "\nYour Pure salary is = " + (Salary - SalaryTax).ToString() +"T";
+
+            labelResult.Text = "Result: " + Result;
+
+            MessageBox.Show(Result,"Result");
+
+            textBoxName.Focus();
+            textBoxName.SelectAll();
+        }
+
+        private void textBoxSalary_TextChanged(object sender, EventArgs e)
+        {
+            CalculateEnable();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxName.Clear();
+            textBoxSalary.Clear();
+            labelResult.Text = "Result:";
+            textBoxName.Focus();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult;
+            
+            dialogResult = MessageBox.Show("Do you want to exit?", "Exit......", MessageBoxButtons.YesNo);
+
+            if (dialogResult==DialogResult.Yes)
+            Application.Exit();
+        }
+    }
+}
