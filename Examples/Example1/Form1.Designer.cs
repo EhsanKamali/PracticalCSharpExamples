9a25f73d13f7d2e727f6412e6156f04f5330fa0f
Ehsan Kamali <EhsanKamali@users.noreply.github.com>
2021-01-14 17:39:36 +0330

    Add Example1
    Add Example1

---
+++ Examples/Example1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Example1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.buttonSayHi = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(105, 12);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 0;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(11, 16);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(88, 13);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Enter Your Name";
+            this.labelName.MouseLeave += new System.EventHandler(this.labelName_MouseLeave);
+            this.labelName.MouseMove += new System.Windows.Forms.MouseEventHandler(this.labelName_MouseMove);
+            // 
+            // buttonSayHi
+            // 
+            this.buttonSayHi.Location = new System.Drawing.Point(14, 58);
+            this.buttonSayHi.Name = "buttonSayHi";
+            this.buttonSayHi.Size = new System.Drawing.Size(81, 28);
+            this.buttonSayHi.TabIndex = 2;
+            this.buttonSayHi.Text = "&Say Hi";
+            this.buttonSayHi.UseVisualStyleBackColor = true;
+            this.buttonSayHi.Click += new System.EventHandler(this.buttonSayHi_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(101, 58);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(81, 28);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "&Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(188, 58);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(81, 28);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.Text = "E&xit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(40, 111);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 13);
+            this.labelResult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 171);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSayHi);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Say Hello";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Button buttonSayHi;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Examples/Example1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSayHi_Click(object sender, EventArgs e)
+        {
+            string Result = "Hello " + textBoxName.Text;
+
+            if (textBoxName.Text != string.Empty)
+            {
+                labelResult.Text = Result;
+                MessageBox.Show(Result);
+            }
+            
+            
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxName.Clear();
+            labelResult.Text = string.Empty;
+            textBoxName.Focus();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void labelName_MouseMove(object sender, MouseEventArgs e)
+        {
+            labelName.ForeColor = Color.Red;
+        }
+
+        private void labelName_MouseLeave(object sender, EventArgs e)
+        {
+            labelName.ForeColor = Color.Black;
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            FormControl();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormControl();
+        }
+
+        private void FormControl()
+        {
+            if (textBoxName.Text == string.Empty)
+            {
+                buttonSayHi.Enabled = false;
+            }
+            else
+            {
+                buttonSayHi.Enabled = true;
+            }
+
+            buttonClear.Enabled = Convert.ToBoolean(textBoxName.Text.Length);
+        }
+    }
+}
