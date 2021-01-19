5bf4dca38c95fe7184a6cbb26c245b0e162d987e
Ehsan Kamali <EhsanKamali@users.noreply.github.com>
2021-01-19 21:41:57 +0330

    Add Example5
    We make a program like as NotePad

---
+++ Examples/CSharp/Example5/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Example5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void SetBackColor(object sender, EventArgs e)
+        {
+            textBoxNotePad.BackColor = Color.FromName(((ToolStripItem)sender).Text);
+
+            foreach (ToolStripMenuItem Tsi in backColorToolStripMenuItem.DropDownItems)
+            {
+                //if (Tsi.Name==((ToolStripItem)sender).Name)
+                if (Tsi.Equals(sender))
+                {
+                    Tsi.Checked = true;
+                }
+                else
+                {
+                    Tsi.Checked = false;
+                }
+            }
+        }
+
+        private void statusBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            statusStrip1.Visible = statusBarToolStripMenuItem.Checked;
+        }
+    }
+}
