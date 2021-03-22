a818a77c2f429e262886587cfcce35c54b7ee5bc
Ehsan Kamali <EhsanKamali@users.noreply.github.com>
2021-03-22 21:48:29 +0430

    Temp

---
+++ Examples/CSharp/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for(int i=0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
